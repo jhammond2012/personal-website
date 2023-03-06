e42c37c69204c269ca075f6c8dba3014c075d3d0
Jonathon Hammond <jonathon@pixel8.io>
2023-03-06 17:59:03 -0500

    added GA

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,7 +5,11 @@ function Header() {
   return (
     <div className='container mx-auto flex justify-between items-center py-12 px-4'>
       <div className='text-6xl font-black'><span className='mr-4'>👋</span>Hey!</div>
-      <div>
+      {/*
+        For now, I have decided to remove this.
+        Once the page is built out a little more I will add it back.
+      */}
+      {/* <div>
         <ul className='lg:flex gap-5 text-xl hidden'>
           <li>About</li>
           <li>Blog</li>
@@ -18,7 +22,7 @@ function Header() {
           <span />
           <span />
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
