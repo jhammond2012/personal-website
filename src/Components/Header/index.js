a0b397e546fb16a8f4c8f1dd4a6f6881fd5bf61f
Jonathon Hammond <jonathon@pixel8.io>
2023-03-06 17:48:19 -0500

    added mobile menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className='container mx-auto flex justify-between items-center py-10 px-4'>
+    <div className='container mx-auto flex justify-between items-center py-12 px-4'>
       <div className='text-6xl font-black'><span className='mr-4'>👋</span>Hey!</div>
       <div>
-        <ul className='flex gap-5 text-xl'>
+        <ul className='lg:flex gap-5 text-xl hidden'>
           <li>About</li>
           <li>Blog</li>
           <li>Contact</li>
         </ul>
+
+        <div id="nav-icon3" className={`${isOpen && 'open'} lg:hidden`} onClick={() => setIsOpen(!isOpen)}>
+          <span />
+          <span />
+          <span />
+          <span />
+        </div>
       </div>
     </div>
   )
--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -12,9 +12,9 @@ function Home() {
   return (
     <>
       <Header />
-      <div className='container mx-auto flex justify-between items-center py-10 flex-col lg:flex-row px-10 lg:px-0'>
-        <div className='w-full lg:w-9/12 mr-10'>
-          <h1 className='text-7xl font-black leading-[80px] tracking-wide mb-8'>Hi! My name is<br />Jonathon</h1>
+      <div className='container mx-auto flex justify-between items-center py-10 flex-col lg:flex-row px-10 lg:px-0 pt-0'>
+        <div className='w-full lg:w-9/12 mr-10 order-2 lg:order-1 mt-16'>
+          <h1 className='text-6xl font-black leading-[80px] tracking-wide mb-8'>Hi! My name is<br />Jonathon</h1>
           <p className='text-2xl leading-normal'>I am a father, a husband and a web developer with over 10 years of experience. I have been fortunate enough to work across many insustries including fitness/nutrition, finance and most recently Web3. I plan on using this as a personal website to showcase my work, write general blog posts as well as tech related tutorials.</p>
           <p className='mt-5 mb-2'>Find me at:</p>
           <ul className='flex gap-3 mb-8'>
@@ -24,7 +24,7 @@ function Home() {
             <li><a href="https://dribbble.com/jonathonhammond" target="_blank" rel="noreferrer"><img src={DribbbleIcon} alt="Dribble" /></a></li>
           </ul>
         </div>
-        <div>
+        <div className='order-1 lg:order-2'>
           <div className='relative'>
             <img src={HarrisonAndJonathon} alt="Jonathon and Harrison" className='rounded-2xl relative z-20' />
             <div className='absolute rounded-2xl bg-[#293D5B] w-full h-full -left-5 top-5 z-10' />
